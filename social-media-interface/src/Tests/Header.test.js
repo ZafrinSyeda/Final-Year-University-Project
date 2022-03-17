6714778e7693ebec86d752ac90b44381a8ae68de
ZafrinSyeda <58555439+ZafrinSyeda@users.noreply.github.com>
2022-03-17 21:22:48 +0000

    removed an automated test that was no longer relevant

--- social-media-interface/src/Tests/Header.test.js
+++ social-media-interface/src/Tests/Header.test.js
@@ -15,12 +15,6 @@ beforeEach(() => {
 });
 
 // testing the interactivity of buttons to work as expected
-it("should show a the upload form pops up on screen when the right button is selected", () => {
-	const postButton = screen.getByText("New Post");
-	fireEvent.click(postButton);
-	expect(screen.getByText("Upload Post")).toBeInTheDocument();
-});
-
 it("should bring a user back to the login page when they click the logout button", () => {
 	const logout = screen.getByText("Logout");
 	fireEvent.click(logout);
